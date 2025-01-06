94a0d7dfd15a0fb750995bf7b3e985775e81827e
Mokerstier <wouter.van.der.heijde@outlook.com>
2025-01-06 15:22:03 +0100

    Refactor Dialog component for improved readability and add background color

--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -2,29 +2,30 @@ import useScrollLock from "@/hooks/useScrollLock";
 import { ReactElement, useEffect, useRef } from "react";
 
 export const Dialog = ({
-  children,
-  showDialog,
+	children,
+	showDialog,
 }: {
-  children: ReactElement[];
-  showDialog: boolean;
+	children: ReactElement[];
+	showDialog: boolean;
 }) => {
-  const dialog = useRef<HTMLDialogElement>(null);
-  useScrollLock();
+	const dialog = useRef<HTMLDialogElement>(null);
+	useScrollLock();
 
-  useEffect(() => {
-    if (showDialog) {
-      dialog.current?.showModal();
-    } else {
-      dialog.current?.close();
-    }
-  }, [showDialog]);
+	useEffect(() => {
+		if (showDialog) {
+			dialog.current?.showModal();
+		} else {
+			dialog.current?.close();
+		}
+	}, [showDialog]);
 
-  return (
-    <dialog
-      className="w-5/6 aspect-auto h-5/6 md:aspect-video rounded-xl"
-      ref={dialog}
-    >
-      {children}
-    </dialog>
-  );
+	return (
+		<dialog
+			className="w-5/6 aspect-auto h-dvh md:aspect-video rounded-xl bg-background"
+			ref={dialog}
+		>
+			{children}
+		</dialog>
+	);
 };
+
