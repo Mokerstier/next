7687e4bc38dfb07e5e52d1dc3d0a8d322c5c9cd4
Mokerstier <wouter.van.der.heijde@outlook.com>
2025-01-06 15:15:46 +0100

    Add Close and Sliders icons as SVG components

---
+++ src/components/icons/CloseIcon.tsx
@@ -0,0 +1,21 @@
+export const CloseIcon = ({ className }: { className?: string }) => {
+	return (
+		<svg
+			className={className}
+			width="48"
+			height="48"
+			viewBox="0 0 48 48"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M36 12L12 36M12 12L36 36"
+				stroke="currentColor"
+				strokeWidth="4"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+};
+
