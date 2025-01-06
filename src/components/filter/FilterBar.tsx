7240527807f5644ae397cd34990f920857da9078
Mokerstier <wouter.van.der.heijde@outlook.com>
2025-01-06 15:22:18 +0100

    Refactor FilterBar component for improved structure and replace FilterIcon with SlidersIcon

--- src/components/filter/FilterBar.tsx
+++ src/components/filter/FilterBar.tsx
@@ -1,29 +1,30 @@
 "use client";
 import { SearchBar } from "@/components/filter/SearchBar";
 import { Filter } from "@/components/filter/Filter";
-import { FilterIcon } from "../icons/FilterIcon";
 import { useState } from "react";
+import { SlidersIcon } from "../icons/SlidersIcon";
 
 export const FilterBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <aside
-      className={`flex flex-col p-4 pb-10 bg-background z-10 w-[300px] min-h-dvh fixed top-0 transition-all border-r border-color-white ${
-        isOpen ? "left-0" : "-left-[300px]"
-      }`}
-    >
-      <div className="relative -z-10">
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label="Show filters"
-          className="absolute bg-background rounded-e-lg p-3 -right-[73px] lg:top-12 top-16 border-e border-white border-b border-t"
-        >
-          <FilterIcon className="w-8 h-8" />
-        </button>
-      </div>
-      <SearchBar />
-      <Filter />
-    </aside>
-  );
+	return (
+		<aside
+			className={`flex flex-col p-4 pb-10 bg-background z-30 w-[300px] min-h-dvh fixed top-0 transition-all border-r border-color-white ${
+				isOpen ? "left-0" : "-left-[300px]"
+			}`}
+		>
+			<div className="relative -z-10">
+				<button
+					onClick={() => setIsOpen(!isOpen)}
+					aria-label="Show filters"
+					className="absolute bg-background rounded-e-lg p-2 -right-[57px] lg:top-12 top-16 border-e border-white border-b border-t"
+				>
+					<SlidersIcon className="w-6 h-6" />
+				</button>
+			</div>
+			<SearchBar />
+			<Filter />
+		</aside>
+	);
 };
+
